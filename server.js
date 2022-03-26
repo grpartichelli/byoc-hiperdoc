b161b329a52453f209a949fdaa6710b790029bab
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-03-26 16:26:45 -0300

    feat: heroku deploy

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(requireHTTPS);
-
-app.use(express.static('./dist/byoc-hiperdoc'));
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/byoc-hiperdoc/'}
-);
-});
+app.use(express.static(__dirname + '/dist/byoc-hiperdoc'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/byoc-hiperdoc/index.html'));});
 app.listen(process.env.PORT || 8080);
