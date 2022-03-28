51edfa1401f66c07384a664eaa9a7fd3036a6f38
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-03-28 19:52:48 -0300

    feat: create sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { ByocToolbarComponent } from './byoc-toolbar/byoc-toolbar.component';
 import { ByocSearchBarComponent } from './byoc-search-bar/byoc-search-bar.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ByocToolbarComponent,
-    ByocSearchBarComponent
-  ],
+  declarations: [AppComponent, ByocToolbarComponent, ByocSearchBarComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -26,8 +22,9 @@ import {MatInputModule} from "@angular/material/input";
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
