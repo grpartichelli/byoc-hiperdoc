ea1b4e8dc7bf1a4600dafb64efd7248e8d989642
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-03-28 17:00:20 -0300

    feat: create toolbar

---
+++ src/app/byoc-toolbar/byoc-toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'byoc-toolbar',
+  templateUrl: './byoc-toolbar.component.html',
+  styleUrls: ['./byoc-toolbar.component.scss']
+})
+export class ByocToolbarComponent {}
