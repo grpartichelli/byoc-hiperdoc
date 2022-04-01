feb99a2dd381a237e7714bfcc76421275a37e0ac
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-01 16:31:11 -0300

    feat: add sidebar

---
+++ src/app/byoc-sidebar/byoc-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'byoc-sidebar',
+  templateUrl: './byoc-sidebar.component.html',
+  styleUrls: ['./byoc-sidebar.component.scss'],
+})
+export class ByocSidebarComponent {
+  @Input() isSidebarOpen = true;
+  constructor() {}
+}
