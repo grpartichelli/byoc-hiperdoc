64788ce9bdd0183a1147127a1d41a6ada6383886
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-02 13:19:23 -0300

    feat: motherboard

--- src/app/data/subsection.data.ts
+++ src/app/data/subsection.data.ts
@@ -45,7 +45,37 @@ export const SubsectionData: Map<string, SubsectionModel> = new Map([
       false
     ),
   ],
-  ['1,2', new SubsectionModel('Motherboard', [], [], [], [])],
+  [
+    '1,2',
+    new SubsectionModel(
+      'Motherboard',
+      [
+        new ParagraphModel(
+          'The Motherboard',
+          'A motherboard is a printed circuit board (PCB) that creates a kind of backbone allowing a variety of components to communicate, and that provides different connectors for components such as the central processing unit (CPU), graphics processing unit (GPU), memory, and storage. Most computers made today, including smartphones, tablets, notebooks, and desktop computers, use motherboards to pull everything together, but the only kind you’ll typically purchase yourself are those made for desktop PCs. ooking at motherboard from the top down, you’ll see a collection of circuits, transistors, capacitors, slots, connectors, heat sinks, and more that all combine to route signals and power throughout the PC and allow you to plug in all of the required components. It’s a complicated product, and many of the technical details are beyond the scope of this how-to. Some of these details are important for your buying decision, though, and we’ll outline them for you below.',
+          'https://www.intel.com.br/content/dam/www/public/us/en/images/photography-consumer/rwd/gaming/rog-strix-z390-e-gaming-motherboard-rwd.png.rendition.intel.web.864.486.png'
+        ),
+        new ParagraphModel(
+          'CPU Compatability',
+          'What CPU are you using with your motherboard?\n' +
+            'The CPU you’re planning on pairing with your board will narrow down your options, since the CPU socket on a given motherboard will only work with the chip line it was designed for.\n' +
+            '\n' +
+            "For instance, if you’re buying an Intel 10th or 11th Generation Core processor, you’ll need a board with an LGA 1200 socket. Older 9th Generation processors need boards with an LGA 1151 socket. AMD makes this process a bit less confusing because (for now at least) the company uses the same AM4 socket for all of its mainstream current-gen chips, from Athlons all the way up to 16-core Ryzen 9 parts, although you may run into complications installing newer CPUs on previous-generation motherboards. Intel, on the other hand, has a tendency in recent years to switch sockets (or at least socket compatibility) from one generation to the next, although that's not the case this generation, with Socket 1200 sticking around for two generations."
+        ),
+        new ParagraphModel(
+          'Sizes',
+          '',
+          'https://cdn.shopify.com/s/files/1/0028/7509/7153/files/Home_1200_x_628.png?v=1616653524'
+        ),
+      ],
+      [],
+      ['https://www.youtube.com/embed/TbGjYumO_aw'],
+      [
+        'https://www.newegg.com/insider/how-to-choose-a-motherboard/',
+        'https://www.tomshardware.com/reviews/motherboard-buying-guide,5682.html',
+      ]
+    ),
+  ],
   ['1,3', new SubsectionModel('CPU', [], [], [], [])],
   ['1,4', new SubsectionModel('Memory', [], [], [], [])],
   ['1,5', new SubsectionModel('Cooler', [], [], [], [])],
