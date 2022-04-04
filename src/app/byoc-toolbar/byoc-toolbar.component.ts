0d4df74101c8d3780c15207b9d041446bf41db5b
grpartichelli <gabriel.partichelli@aegro.com.br>
2022-04-04 18:55:47 -0300

    feat: create dialog to learn

--- src/app/byoc-toolbar/byoc-toolbar.component.ts
+++ src/app/byoc-toolbar/byoc-toolbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { GuidedService } from '../service/guided.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ByocVideoComponent } from '../byoc-video/byoc-video.component';
 
 @Component({
   selector: 'byoc-toolbar',
@@ -10,7 +12,10 @@ export class ByocToolbarComponent implements OnInit {
   @Output() isSidebarOpenEvent = new EventEmitter<boolean>();
   isSidebarOpen = true;
 
-  constructor(private readonly guidedService: GuidedService) {}
+  constructor(
+    private readonly guidedService: GuidedService,
+    private readonly matDialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.isSidebarOpenEvent.emit(true);
@@ -22,6 +27,10 @@ export class ByocToolbarComponent implements OnInit {
 
   public get isGuidedActive() {
     return this.guidedService.isActive;
+  }
+
+  public howToUse() {
+    this.matDialog.open(ByocVideoComponent);
   }
 
   toggleSidebar() {
